cfe0ef833e9f5bc5ccef4ba316457d7c3fd5739c
Myoungdo Park <cuspymd@gmail.com>
2023-09-12 22:12:21 +0900

    Set up assembler

---
+++ hack_assembler/assembler.py
@@ -0,0 +1,6 @@
+import os
+
+
+def run(file_path: str):
+    folder_path, file_name = os.path.split(file_path)
+    
---
+++ hack_assembler/test_assembler.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+import hack_assembler.assembler as assembler
+
+
+class TestAssembler(unittest.TestCase):
+    def test_assembler_given_no_symbol(self):
+        OUT_FILE = "./test_data/RectL.hack"
+        SOLUTION_FILE = "./test_data/SolutionL.hack"
+        if os.path.exists(OUT_FILE):
+            os.remove(OUT_FILE)
+
+        assembler.run("./test_data/RectL.asm")
+        self.assertTrue(os.path.exists(OUT_FILE))
+
+        with open(SOLUTION_FILE, 'r') as solution_file:
+            solution_text = solution_file.read()
+
+        with open(OUT_FILE, 'r') as file:
+            output_text = file.read()
+
+        os.remove(OUT_FILE)
+        self.assertEqual(output_text, solution_text)
